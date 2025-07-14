6aeeec226dd8e32dc8e3799285cd05ae0311c591
ringmaker <chrisringmaker@gmail.com>
2025-07-14 16:34:09 -0400

    console log tree structure

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@
  * Imports
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
-// Node is imported in the Tree file
 import { Tree } from './tree.js'
+import { Node } from './node.js'
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 
 /*
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -19,10 +21,22 @@ import { Tree } from './tree.js'
  */
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
- * Functions go here
- *
- * Descriptions of the major functions or classes go here if necessary
+ * Driver Functions
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 
-function name(params) {}
+console.log('Original Array', tree.arr)
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+	if (!node) return
+	if (node.right !== null) {
+		prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false)
+	}
+	console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`)
+	if (node.left !== null) {
+		prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true)
+	}
+}
+
+console.log('\nTree structure:')
+prettyPrint(tree.root)
