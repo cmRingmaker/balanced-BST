18acde69966a3dd2c1a89b57d59068f83bb632fb
ringmaker <chrisringmaker@gmail.com>
2025-07-17 19:25:09 -0400

    info first pass, functionality complete but unfinished styling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+// Make this user-changable later
+// const defaultArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+const defaultArray = [
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+	33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61,
+	62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+	91, 92, 93, 94, 95, 96, 97, 98, 99, 100,
+]
+
+const tree = new Tree(defaultArray)
+
 /*
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  * Imports
@@ -13,7 +24,7 @@ import { Node } from './node.js'
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 
-document.addEventListener('DOMContentLoaded', initializeTree)
+document.addEventListener('DOMContentLoaded', initializeTree(), initializeInfo())
 
 /*
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -50,17 +61,27 @@ function prettyPrint(node, prefix = '', isLeft = true) {
 }
 
 function initializeTree() {
-	// Make this user-changable later
-	// const defaultArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-	const defaultArray = [
-		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-		32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-		61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
-		90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100,
-	]
-	const tree = new Tree(defaultArray)
-
 	// Render tree in output
 	const output = document.getElementById('tree-output')
 	output.textContent = prettyPrint(tree.root)
 }
+
+function initializeInfo() {
+	// Render information in Info
+
+	// Target the id to insert into, and use the corresponding method from the tree
+	const traverse = [
+		{ id: 'level-order', method: 'levelOrder' },
+		{ id: 'in-order', method: 'inOrder' },
+		{ id: 'pre-order', method: 'preOrder' },
+		{ id: 'post-order', method: 'postOrder' },
+	]
+
+	traverse.forEach(({ id, method }) => {
+		const res = []
+		tree[method]((node) => res.push(node.data))
+		document.getElementById(id).querySelector('span').textContent = `[${res.join(', ')}]`
+	})
+}
+
+// initializeInfo()
